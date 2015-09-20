82ff4d75032f6102847dead3ad607ac986b0ffae
Andrew Dinh <andrewdinh74@yahoo.com>
2015-09-19 19:04:25 -0700

    Cannot get it up and running yet

---
+++ ex1_createServer.js
@@ -0,0 +1,17 @@
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  port: Number(process.argv[2] || 8080)
+});
+
+server.route({path: '/', method: 'GET', handler: function handler(request, reply){
+  if (request.path === '/'){
+    console.log('Hello hapi');
+  }
+  reply({mustFlow: true});
+}});
+server.start(function () {
+  console.log('Server running at:', server.info.uri);
+});
